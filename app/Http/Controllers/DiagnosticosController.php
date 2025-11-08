365cb1b7c42d07554a146639081a031f6bc76103
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2025-11-08 18:28:37 -0300

    feat: Implement CRUD operations for Diagnosticos with validation

--- app/Http/Controllers/DiagnosticosController.php
+++ app/Http/Controllers/DiagnosticosController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreDiagnosticoRequest;
+use App\Http\Requests\UpdateDiagnosticoRequest;
 use App\Services\DiagnosticosService;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class DiagnosticosController extends Controller
 {
@@ -12,4 +15,42 @@ class DiagnosticosController extends Controller
         //
     }
 
+    public function index(): JsonResponse
+    {
+        return response()->json($this->diagnosticosService->all());
+    }
+
+    public function show(int $id): JsonResponse
+    {
+        return response()->json($this->diagnosticosService->find($id));
+    }
+
+    public function store(StoreDiagnosticoRequest $request): JsonResponse
+    {
+        $diagnostico = $this->diagnosticosService->create($request->validated());
+
+        return response()->json($diagnostico, Response::HTTP_CREATED);
+    }
+
+    public function update(int $id, UpdateDiagnosticoRequest $request): JsonResponse
+    {
+        $data = $request->validated();
+
+        if ($data === []) {
+            return response()->json([
+                'message' => 'Nenhum dado foi informado.',
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        $diagnostico = $this->diagnosticosService->update($id, $data);
+
+        return response()->json($diagnostico);
+    }
+
+    public function destroy(int $id): JsonResponse
+    {
+        $this->diagnosticosService->delete($id);
+
+        return response()->json(null, Response::HTTP_NO_CONTENT);
+    }
 }
