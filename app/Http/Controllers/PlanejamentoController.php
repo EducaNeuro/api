bcbc3cd58837b463e27bda0cabd4538aa122764b
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2025-11-08 18:28:40 -0300

    feat: Implement CRUD operations for Planejamento with validation

--- app/Http/Controllers/PlanejamentoController.php
+++ app/Http/Controllers/PlanejamentoController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StorePlanejamentoRequest;
+use App\Http\Requests\UpdatePlanejamentoRequest;
 use App\Services\PlanejamentoService;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class PlanejamentoController extends Controller
 {
@@ -12,4 +15,42 @@ class PlanejamentoController extends Controller
         //
     }
 
+    public function index(): JsonResponse
+    {
+        return response()->json($this->planejamentoService->all());
+    }
+
+    public function show(int $id): JsonResponse
+    {
+        return response()->json($this->planejamentoService->find($id));
+    }
+
+    public function store(StorePlanejamentoRequest $request): JsonResponse
+    {
+        $planejamento = $this->planejamentoService->create($request->validated());
+
+        return response()->json($planejamento, Response::HTTP_CREATED);
+    }
+
+    public function update(int $id, UpdatePlanejamentoRequest $request): JsonResponse
+    {
+        $data = $request->validated();
+
+        if ($data === []) {
+            return response()->json([
+                'message' => 'Nenhum dado foi informado.',
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        $planejamento = $this->planejamentoService->update($id, $data);
+
+        return response()->json($planejamento);
+    }
+
+    public function destroy(int $id): JsonResponse
+    {
+        $this->planejamentoService->delete($id);
+
+        return response()->json(null, Response::HTTP_NO_CONTENT);
+    }
 }
