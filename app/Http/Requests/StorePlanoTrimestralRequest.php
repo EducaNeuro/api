87cbe20df951592bfbe0dba5f35b49490ba04822
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2025-11-08 18:28:56 -0300

    feat: Create StorePlanoTrimestralRequest for validating Plano Trimestral data

---
+++ app/Http/Requests/StorePlanoTrimestralRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class StorePlanoTrimestralRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+            'planejamento_id' => ['required', 'integer', 'exists:planejamento,id'],
+            'objetivo' => ['nullable', 'string'],
+            'estrategias' => ['nullable', 'string'],
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'error' => true,
+            'message' => $validator->errors()->first(),
+        ], 422));
+    }
+}
