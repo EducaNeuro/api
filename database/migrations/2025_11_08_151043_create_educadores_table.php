ce60adac862dd45ea33fad4ec165d89f36155a92
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2025-11-08 12:23:20 -0300

    create migration for educadores table

---
+++ database/migrations/2025_11_08_151043_create_educadores_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use function Laravel\Prompts\table;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('educadores', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('email', 255)->unique();
+            $table->string('cpf', 14)->unique();
+            $table->string('telefone', 15);
+            $table->string('senha', 255);
+            $table->string('disciplina', 50);
+            $table->string('turno', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('educadores');
+    }
+};
