da4e76b60888bc642d6a95f40c08b2d9ff1bda6c
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2025-11-08 12:23:53 -0300

    create migration for entrevista_familiar table

---
+++ database/migrations/2025_11_08_151430_create_entrevista_familiar_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('entrevista_familiar', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->foreignId('aluno_id')
+                ->constrained('alunos')
+                ->cascadeOnDelete();
+            $table->text('gestacao_e_primeiros_meses');
+            $table->text('sociabilidade');
+            $table->text('comportamento');
+            $table->boolean('sensibilidade_sensorial');
+            $table->boolean('quadro_convulsivo');
+            $table->boolean('uso_medicamento');
+            $table->text('composicao_familiar');
+            $table->text('interesses_pessoais');
+            $table->string('servicos_apoio', 255)->nullable();
+            $table->timestamps();
+
+            $table->index('aluno_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('entrevista_familiar');
+    }
+};
