575950502120c35b5648589f6652cfdf238afea2
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2025-11-08 18:29:51 -0300

    feat: Implement CRUD methods in RegistroPedagogicoService for managing Registro Pedagogico records

--- app/Services/RegistroPedagogicoService.php
+++ app/Services/RegistroPedagogicoService.php
@@ -2,9 +2,38 @@
 
 namespace App\Services;
 
+use App\Models\RegistroPedagogico;
 use App\Repositories\RegistroPedagogicoRepository;
 
 class RegistroPedagogicoService
 {
     public function __construct(private readonly RegistroPedagogicoRepository $registroPedagogicoRepository) {}
+
+    public function all()
+    {
+        return $this->registroPedagogicoRepository->all();
+    }
+
+    public function find(int $id): RegistroPedagogico
+    {
+        return $this->registroPedagogicoRepository->findOrFail($id);
+    }
+
+    public function create(array $data): RegistroPedagogico
+    {
+        return $this->registroPedagogicoRepository->create($data);
+    }
+
+    public function update(int $id, array $data): RegistroPedagogico
+    {
+        $registro = $this->registroPedagogicoRepository->findOrFail($id);
+
+        return $this->registroPedagogicoRepository->update($registro, $data);
+    }
+
+    public function delete(int $id): void
+    {
+        $registro = $this->registroPedagogicoRepository->findOrFail($id);
+        $this->registroPedagogicoRepository->delete($registro);
+    }
 }
