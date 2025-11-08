d4777b4d518e06e68cb74b4df109a7e496ceb8be
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2025-11-08 18:29:07 -0300

    feat: Create UpdateDiagnosticoRequest for validating Diagnostico updates

---
+++ app/Http/Requests/UpdateDiagnosticoRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class UpdateDiagnosticoRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+            'nome' => ['sometimes', 'string', 'max:255'],
+            'aluno_id' => ['sometimes', 'integer', 'exists:alunos,id'],
+            'observacoes' => ['sometimes', 'nullable', 'string'],
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'error' => true,
+            'message' => $validator->errors()->first(),
+        ], 422));
+    }
+}
