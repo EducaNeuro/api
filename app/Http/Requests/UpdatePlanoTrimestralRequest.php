b1f6f5fa67cc2795f21bb3a5db722d70baf88c1b
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2025-11-08 18:29:17 -0300

    feat: Create UpdatePlanoTrimestralRequest for validating Plano Trimestral updates

---
+++ app/Http/Requests/UpdatePlanoTrimestralRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class UpdatePlanoTrimestralRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+            'planejamento_id' => ['sometimes', 'integer', 'exists:planejamento,id'],
+            'objetivo' => ['sometimes', 'nullable', 'string'],
+            'estrategias' => ['sometimes', 'nullable', 'string'],
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'error' => true,
+            'message' => $validator->errors()->first(),
+        ], 422));
+    }
+}
