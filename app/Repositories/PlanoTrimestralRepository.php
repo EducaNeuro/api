e0d9b43232680d95ad7749204ec8beb7e1dcca8d
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2025-11-08 18:29:35 -0300

    feat: Implement CRUD methods in PlanoTrimestralRepository for managing Plano Trimestral records

--- app/Repositories/PlanoTrimestralRepository.php
+++ app/Repositories/PlanoTrimestralRepository.php
@@ -1,7 +1,35 @@
 <?php
 namespace App\Repositories;
 
+use App\Models\PlanoTrimestral;
+
 class PlanoTrimestralRepository
 {
-    //
+    public function all()
+    {
+        return PlanoTrimestral::with('planejamento')->get();
+    }
+
+    public function create(array $data): PlanoTrimestral
+    {
+        return PlanoTrimestral::create($data);
+    }
+
+    public function findOrFail(int $id): PlanoTrimestral
+    {
+        return PlanoTrimestral::with('planejamento')->findOrFail($id);
+    }
+
+    public function update(PlanoTrimestral $planoTrimestral, array $data): PlanoTrimestral
+    {
+        $planoTrimestral->fill($data);
+        $planoTrimestral->save();
+
+        return $planoTrimestral;
+    }
+
+    public function delete(PlanoTrimestral $planoTrimestral): void
+    {
+        $planoTrimestral->delete();
+    }
 }
