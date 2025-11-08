a675bebc3aee1c1328598839db55efc718b1dcd5
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2025-11-08 18:28:50 -0300

    feat: Create StoreDiagnosticoRequest for validating Diagnostico data

---
+++ app/Http/Requests/StoreDiagnosticoRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class StoreDiagnosticoRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+            'nome' => ['required', 'string', 'max:255'],
+            'aluno_id' => ['required', 'integer', 'exists:alunos,id'],
+            'observacoes' => ['nullable', 'string'],
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'error' => true,
+            'message' => $validator->errors()->first(),
+        ], 422));
+    }
+}
